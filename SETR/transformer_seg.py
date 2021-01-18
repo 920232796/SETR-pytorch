7875cd80e910ccb06773fba7334eeca100bc1e31
xingzhaohu <920232796@qq.com>
2021-01-18 18:15:06 +0800

    add mnist example

--- SETR/transformer_seg.py
+++ SETR/transformer_seg.py
@@ -9,7 +9,7 @@ from einops import rearrange
 from SETR.transformer_model import TransModel2d, TransConfig
 
 class Encoder2D(nn.Module):
-    def __init__(self, config: TransConfig):
+    def __init__(self, config: TransConfig, is_segmentation=True):
         super().__init__()
         self.config = config
         self.out_channels = config.out_channels
@@ -20,6 +20,7 @@ class Encoder2D(nn.Module):
         self.hh = self.img_size[0] // 16
         self.ww = self.img_size[1] // 16
 
+        self.is_segmentation = is_segmentation
     def forward(self, x):
         ## x:(b, c, w, h)
         b, c, h, w = x.shape
@@ -39,6 +40,8 @@ class Encoder2D(nn.Module):
         x = rearrange(x, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = p1, p2 = p2)
         
         encode_x = self.bert_model(x)[-1] # 取出来最后一层
+        if not self.is_segmentation:
+            return encode_x
 
         x = self.final_dense(encode_x)
         x = rearrange(x, "b (h w) (p1 p2 c) -> b c (h p1) (w p2)", p1 = self.hh, p2 = self.ww, h = hh, w = ww, c = self.config.hidden_size)
@@ -46,7 +49,7 @@ class Encoder2D(nn.Module):
 
 
 class PreTrainModel(nn.Module):
-    def __init__(self, img_size=, 
+    def __init__(self, img_size, 
                         in_channels, 
                         out_class, 
                         hidden_size=1024, 
@@ -60,15 +63,38 @@ class PreTrainModel(nn.Module):
                             hidden_size=hidden_size, 
                             num_hidden_layers=num_hidden_layers, 
                             num_attention_heads=num_attention_heads)
-        self.encoder_2d = Encoder2D(config)
+        self.encoder_2d = Encoder2D(config, is_segmentation=False)
         self.cls = nn.Linear(hidden_size, out_class)
 
     def forward(self, x):
-        encode_img, _ = self.encoder_2d(x)
+        encode_img = self.encoder_2d(x)
         encode_pool = encode_img.mean(dim=1)
         out = self.cls(encode_pool)
         return out 
 
+class Vit(nn.Module):
+    def __init__(self, img_size, 
+                        in_channels, 
+                        out_class, 
+                        hidden_size=1024, 
+                        num_hidden_layers=8, 
+                        num_attention_heads=16,
+                        decode_features=[512, 256, 128, 64]):
+        super().__init__()
+        config = TransConfig(img_size=img_size, 
+                            in_channels=in_channels, 
+                            out_channels=0, 
+                            hidden_size=hidden_size, 
+                            num_hidden_layers=num_hidden_layers, 
+                            num_attention_heads=num_attention_heads)
+        self.encoder_2d = Encoder2D(config, is_segmentation=False)
+        self.cls = nn.Linear(hidden_size, out_class)
+
+    def forward(self, x):
+        encode_img = self.encoder_2d(x)
+        encode_pool = encode_img.mean(dim=1)
+        out = self.cls(encode_pool)
+        return out 
 
 class Decoder2D(nn.Module):
     def __init__(self, in_channels, out_channels, features=[512, 256, 128, 64]):
---
+++ task_minst.py
@@ -0,0 +1,90 @@
+import torch 
+from SETR.transformer_seg import SETRModel, Vit
+import torchvision
+import torch
+import torch.nn as nn 
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt 
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device is " + str(device))
+
+def compute_acc(model, test_dataloader):
+    with torch.no_grad():
+        right_num = 0
+        total_num = 0
+        for in_data, label in tqdm(test_dataloader, total=len(test_dataloader)):
+            in_data = in_data.to(device)
+            label = label.to(device)
+            total_num += len(in_data)
+            out = model(in_data)
+            pred = out.argmax(dim=-1)
+            for i, each_pred in enumerate(pred):
+                if int(each_pred) == int(label[i]):
+                    right_num += 1
+        
+        return (right_num / total_num)
+
+if __name__ == "__main__":
+
+    model = Vit(img_size=(7, 7), 
+                    in_channels=1, 
+                    out_class=10, 
+                    hidden_size=1024, 
+                    num_hidden_layers=1, 
+                    num_attention_heads=16, 
+                    decode_features=[512, 256, 128, 64])
+    print(model)
+    model.to(device)
+    
+    transform = transforms.Compose([transforms.ToTensor(),
+                               transforms.Normalize(mean=[0.5],std=[0.5])])
+    data_train = datasets.MNIST(root = "./data/",
+                            transform=transform,
+                            train = True,
+                            download = True)
+
+    data_test = datasets.MNIST(root="./data/",
+                           transform = transform,
+                           train = False)
+
+    data_loader_train = torch.utils.data.DataLoader(dataset=data_train,
+                                                batch_size = 64,
+                                                shuffle = True)
+
+    data_loader_test = torch.utils.data.DataLoader(dataset=data_test,
+                                                batch_size = 32,
+                                                shuffle = True)
+
+    optimizer = torch.optim.Adam(model.parameters())
+    loss_func = nn.CrossEntropyLoss()
+    report_loss = 0
+    step = 0
+    best_acc = 0.0
+
+    for in_data, label in tqdm(data_loader_train, total=len(data_loader_train)):
+        batch_size = len(in_data)
+        in_data = in_data.to(device)
+        label = label.to(device)
+        optimizer.zero_grad()
+        step += 1
+        out = model(in_data)
+        loss = loss_func(out, label)
+        loss.backward()
+        optimizer.step()
+        report_loss += loss.item()
+        if step % 10 == 0:
+            print("report_loss is : " + str(report_loss))
+            report_loss = 0
+            acc = compute_acc(model, data_loader_test)
+            if acc > best_acc:
+                best_acc = acc 
+                torch.save(model.state_dict(), "./mnist_model.pkl")
+
+            print("acc is " + str(acc) + ", best acc is " + str(best_acc))
+
+            
+    
+
+        
