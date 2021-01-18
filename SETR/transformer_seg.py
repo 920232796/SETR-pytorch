99f736ebf27adc99765094a54702496fa0aacbf1
xingzhaohu <920232796@qq.com>
2021-01-18 15:58:57 +0800

    init

---
+++ SETR/transformer_seg.py
@@ -0,0 +1,106 @@
+import logging
+import math
+import os
+
+import torch
+from torch import nn
+from torch.nn import CrossEntropyLoss, MSELoss
+from einops import rearrange
+from SETR.transformer_model import TransModel2d, TransConfig
+
+class Encoder2D(nn.Module):
+    def __init__(self, config: TransConfig):
+        super().__init__()
+        self.config = config
+        self.out_channels = config.out_channels
+        self.bert_model = TransModel2d(config)
+        assert config.img_size[0] * config.img_size[1] * config.hidden_size % 256 == 0, "不能除尽"
+        self.final_dense = nn.Linear(config.hidden_size, config.img_size[0] * config.img_size[1] * config.hidden_size // 256)
+        self.img_size = config.img_size
+        self.hh = self.img_size[0] // 16
+        self.ww = self.img_size[1] // 16
+
+    def forward(self, x):
+        ## x:(b, c, w, h)
+        b, c, h, w = x.shape
+        assert self.config.in_channels == c, "in_channels != 输入图像channel"
+        p1 = self.img_size[0]
+        p2 = self.img_size[1]
+
+        if h % p1 != 0:
+            print("请重新输入img size 参数 必须整除")
+            os._exit(0)
+        if w % p2 != 0:
+            print("请重新输入img size 参数 必须整除")
+            os._exit(0)
+        hh = h // p1 
+        ww = w // p2 
+
+        x = rearrange(x, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = p1, p2 = p2)
+        
+        x = self.bert_model(x)[-1] # 取出来最后一层
+        x = self.final_dense(x)
+        x = rearrange(x, "b (h w) (p1 p2 c) -> b c (h p1) (w p2)", p1 = self.hh, p2 = self.ww, h = hh, w = ww, c = self.config.hidden_size)
+        return x 
+
+
+class Decoder2D(nn.Module):
+    def __init__(self, in_channels, out_channels, features=[512, 256, 128, 64]):
+        super().__init__()
+        self.decoder_1 = nn.Sequential(
+                    nn.Conv2d(in_channels, features[0], 3, padding=1),
+                    nn.BatchNorm2d(features[0]),
+                    nn.ReLU(inplace=True),
+                    nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+                )
+        self.decoder_2 = nn.Sequential(
+                    nn.Conv2d(features[0], features[1], 3, padding=1),
+                    nn.BatchNorm2d(features[1]),
+                    nn.ReLU(inplace=True),
+                    nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+                )
+        self.decoder_3 = nn.Sequential(
+            nn.Conv2d(features[1], features[2], 3, padding=1),
+            nn.BatchNorm2d(features[2]),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        )
+        self.decoder_4 = nn.Sequential(
+            nn.Conv2d(features[2], features[3], 3, padding=1),
+            nn.BatchNorm2d(features[3]),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        )
+
+        self.final_out = nn.Conv2d(features[-1], out_channels, 3, padding=1)
+
+    def forward(self, x):
+        x = self.decoder_1(x)
+        x = self.decoder_2(x)
+        x = self.decoder_3(x)
+        x = self.decoder_4(x)
+        x = self.final_out(x)
+        return x
+
+class SETRModel(nn.Module):
+    def __init__(self, config: TransConfig):
+        super().__init__()
+        self.encoder_2d = Encoder2D(config)
+        self.decoder_2d = Decoder2D(in_channels=config.hidden_size, out_channels=config.out_channels, features=[512, 256, 128, 64])
+
+    def forward(self, x):
+        x = self.encoder_2d(x)
+        x = self.decoder_2d(x)
+        return x 
+
+if __name__ == "__main__":
+    
+    config = TransConfig(img_size=(32, 32), in_channels=3, out_channels=1, hidden_size=1024, num_hidden_layers=8, num_attention_heads=16)
+
+    t1 = torch.rand(1, 3, 256, 256)
+    net = SETRModel(config)
+    print(net)
+    print(net(t1).shape)
+
+   
+
